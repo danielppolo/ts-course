e6a32e75eed594f23718ac03a5e78645e7ddcb85
Daniel Polo <od.polo@gmail.com>
2021-07-29 13:10:03 -0500

    feat: Decorators

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,28 @@
 "use strict";
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
+        var _this = this;
+        this.attach = function () {
+            _this.hostElement.insertAdjacentElement('afterbegin', _this.element);
+        };
+        this.submitHandler = function (event) {
+            console.log('Submitted', _this.titleInputElement.value);
+            event.preventDefault();
+        };
+        this.configure = function () {
+            _this.element.addEventListener('submit', _this.submitHandler);
+        };
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
     }
-    ProjectInput.prototype.attach = function () {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
-    };
     return ProjectInput;
 }());
 var f = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,11 +12,28 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
-  private attach() {
+  private attach = () => {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private submitHandler = (event: Event) => {
+    console.log('Submitted', this.titleInputElement.value);
+    event.preventDefault();
+
+  }
+
+  private configure = () => {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 }
 
